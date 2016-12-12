a22387a7d0ba1c49c33465cf160100f9218e403a
andela-oolutola <oreofeoluwapo.olutola@andela.com>
2016-12-12 12:24:36 +0100

    Add dashboard & login routes & components

--- src/main.js
+++ src/main.js
@@ -1,11 +1,36 @@
 import Vue from 'vue';
-import App from './App';
-// import VueRouter from 'vue-router';
+import VueRouter from 'vue-router';
 
-// Vue.use(VueRouter);
+import App from './App';
+import Login from './components/login';
+import Dashboard from './components/dashboard';
+
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: '/login',
+    component: Login,
+  },
+  {
+    path: '/dashboard',
+    component: Dashboard,
+  },
+];
+
+const router = new VueRouter({
+  routes,
+});
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   render: h => h(App),
+  router,
 });
+
+
+// new Vue({
+//   el: '#app',
+//   render: h => h(App),
+// });
